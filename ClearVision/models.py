1b98a24566adc9450ec0634e94a1a071e4608446
rotarycut <shermanyong@gmail.com>
2015-06-18 00:43:05 +0800

    First Commit - Changed the database connections, models. Cant get psycopg2 plugin to work.

--- ClearVision/models.py
+++ ClearVision/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Appointment(models.Model):
+    appt_type = models.CharField(max_length=200)
+    date = models.DateTimeField('Appointment date and time')
+    clinic = models.CharField(max_length=200)
+    creation_time = models.DateTimeField('Creation Time')
+
+class Patient(models.Model):
+    Patient = models.ForeignKey(Appointment)
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    contact = models.CharField(max_length=50)
+    dob = models.CharField(max_length=50)
+
+class Doctor(models.Model):
+    Doctor = models.ForeignKey(Appointment)
+    name = models.CharField(50)
+    phoneModel = models.CharField(50)
+    calDavAccount = models.CharField(50)
+
+class Nurse(models.Model):
+    id = models.CharField(50)
+    name = models.CharField(50)
--- FYP/settings.py
+++ FYP/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'FYP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'schedules',
+        'USER': 'sherman',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
