76f7ff88624780ab41f606ec06e8ab35bd63baf7
rotarycut <shermanyong@gmail.com>
2015-06-22 00:08:58 +0800

    Login set up.
    Test it with test data below:
    
    username: sherman
    password: 12345
    ----
    2nd commit with same update as above as I forgot to add these 2 files for commit

--- ClearVision/views.py
+++ ClearVision/views.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import Context
+
+
 
 @login_required
 def success(request):
-    return HttpResponse("Hello, world. You're at the Clearvision home page and successfully logged in.")
+    response = "Hello, world. You're at the Clearvision home page and successfully logged in."
+    context = Context({
+        'message': response
+    })
+    return render(request, 'success.html', context)
