d9319e757abdefb8410e394493ffd26f6ffec9c8
rotarycut <shermanyong@gmail.com>
2015-06-22 00:41:45 +0800

    Refined login/logout

--- ClearVision/urls.py
+++ ClearVision/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url(r'^success', views.success, name='success'),
+    url(r'^logout', views.logout, name='logout'),
 ]
--- ClearVision/views.py
+++ ClearVision/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.contrib.auth.views import logout_then_login
 from django.shortcuts import render
 from django.template import Context
 
@@ -9,6 +9,9 @@ from django.template import Context
 def success(request):
     response = "Hello, world. You're at the Clearvision home page and successfully logged in."
     context = Context({
-        'message': response
+        'message': response,
     })
     return render(request, 'success.html', context)
+
+def logout(request):
+    return logout_then_login(request, 'login')
