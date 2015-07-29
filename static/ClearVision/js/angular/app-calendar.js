1d2019cd6b0e45bd31104257d36b8bb1d5962f27
carinahuqunqun <qunqun.hu.2012@sis.smu.edu.sg>
2015-07-30 02:07:49 +0800

    added legend, modified form into angular js

--- static/ClearVision/js/angular/app-calendar.js
+++ static/ClearVision/js/angular/app-calendar.js
@@ -8,6 +8,8 @@ appCalendar.controller('CalendarCtrl', function ($scope, $compile, uiCalendarCon
     var m = date.getMonth();
     var y = date.getFullYear();
 
+
+
     $scope.changeTo = 'Hungarian';
     /* event source that pulls from google.com */
     $scope.eventSource = {
@@ -384,6 +386,76 @@ appCalendar.controller('CalendarCtrl', function ($scope, $compile, uiCalendarCon
             .success(function (data) {
                 console.log("Successfully updated");
             });
+    };
+
+    /* Start of date picker codes */
+    $scope.datepickers = {
+        showDatePicker: false
+    };
+    $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 7 ) );
+    };
+
+    $scope.today = function () {
+        $scope.datePickerCalendar = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.datePickerCalendar = null;
+    };
+
+    $scope.toggleMin = function () {
+        $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function ($event, which) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.datepickers[which] = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1,
+        showWeeks: 'false'
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+    /* End of date picker codes */
+
+
+    $scope.listOfAppointmentTypes = ["Pre-Evaluation", "Lasik Surgery", "Screening", "Follow-up"];
+
+    $scope.listOfAppointmentTimings = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00"];
+
+    $scope.listOfMarketingChannels = ["Email", "Friend", "Facebook Advertisement", "Clearvision Website"];
+
+    $scope.listOfTimeslotPatients = ["Amabel Lau", "Leon Lim", "Sherman Yong"];
+
+    $scope.clearForm = function () {
+        $scope.appointmentType = "";
+        $scope.apptDateTime = "";
+        $scope.appointmentTime = "";
+        $scope.patientName = "";
+        $scope.patientContact = "";
+        $scope.doctorAssigned = "";
+        $scope.marketingChannel = "";
+        $scope.appointmentRemarks = "";
+    };
+    $scope.showForm = function () {
+        $scope.formAnimation = "form-container-show";
+        $scope.calAnimation = "cal-container-resize";
+        $scope.topAnimation = "top-container-resize";
+    };
+
+    $scope.hideForm = function () {
+        $scope.formAnimation = "form-container";
+        $scope.calAnimation = "cal-container";
+        $scope.topAnimation = "top-container";
     };
 
 
