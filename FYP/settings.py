4c4d61a2d460f7530abd6a4a2eeec1409bad41c1
rotarycut <shermanyong@gmail.com>
2015-08-14 01:26:26 +0800

    Prep settings.py for deployment

--- FYP/settings.py
+++ FYP/settings.py
@@ -130,5 +130,5 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'ClearVision/static'
 """
