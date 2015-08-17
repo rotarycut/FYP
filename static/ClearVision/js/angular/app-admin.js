cfae7231474cfd0b0f6be9c40622ae9b4504de24
carinahuqunqun <qunqun.hu.2012@sis.smu.edu.sg>
2015-08-17 15:33:45 +0800

    fixed calendar view button active state issue

--- static/ClearVision/js/angular/app-admin.js
+++ static/ClearVision/js/angular/app-admin.js
@@ -1,8 +1,4 @@
 var appAdmin = angular.module('app.admin', []);
 app.controller('adminCtrl', function($scope) {
-    $scope.master = {firstName: "John", lastName: "Doe"};
-    $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-    };
-    $scope.reset();
+
 });
