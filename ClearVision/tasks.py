0171b573e88b7c1ff78964036342ef567f9240a9
rotarycut <shermanyong@gmail.com>
2015-08-31 20:39:23 +0800

    Added Celerybeat daemon to fire process - pending RabbitMQ addition

--- ClearVision/tasks.py
+++ ClearVision/tasks.py
@@ -1,13 +1,19 @@
 from celery import task
-import datetime
+from datetime import date
 from ClearVision.models import *
 
 @task()
 def sendSMS():
-    today_patient_data = Appointment.objects.filter(timeBucket__date=datetime.today()).values('patients', 'patients__name',
+    today_patient_data = Appointment.objects.filter(timeBucket__date=date.today()).values('patients', 'patients__name',
                                                                                              'patients__gender', 'timeBucket__date',
                                                                                              'timeBucket__start', 'apptType', 'id',
                                                                                              'timeBucket', 'doctor__name', 'clinic',
                                                                                              'doctor').\
             exclude(patients__isnull=True)
-    print(today_patient_data)
+
+    numbersToSend = []
+
+    for eachObj in today_patient_data:
+        numbersToSend.append(eachObj['patients'])
+
+    print(numbersToSend)
