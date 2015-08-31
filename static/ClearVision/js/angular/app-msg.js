6052494fd8b7a24531c8ae341ce435ce5767eaf4
carinahuqunqun <qunqun.hu.2012@sis.smu.edu.sg>
2015-08-31 14:09:00 +0800

    updated message inbox page

--- static/ClearVision/js/angular/app-msg.js
+++ static/ClearVision/js/angular/app-msg.js
@@ -2,6 +2,18 @@ var appMsgLog = angular.module('app.msgLog', []);
 
 appMsgLog.controller('msgCtrl', function ($scope, $http) {
 
+    $scope.mails = [
+        {
+            name: 'Philip Horbacheuski',
+            title: 'Hi, Welcome to Google Mail',
+            datetime: '2015-08-25, 12:30:00',
+        },
+        {
+            name: 'Nikola Foley',
+            title: 'Hi, Welcome to Google Mail',
+            datetime: '2015-08-25, 12:30:00',
+        }
+    ];
 
-
+    $scope.mailStarred = 'mail-unstarred';
 });
