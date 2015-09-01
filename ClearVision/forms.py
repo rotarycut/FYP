d28632ee53c99460e6ccff47b265599799f3d35d
rotarycut <shermanyong@gmail.com>
2015-09-01 19:43:07 +0800

    Forms commit

--- ClearVision/forms.py
+++ ClearVision/forms.py
@@ -1,12 +1,7 @@
 __author__ = 'sherman'
 from django import forms
 
-class CreateForm(forms.Form):
-    patient_name = forms.CharField(label='Patient Name', max_length=100)
-    patient_gender = forms.CharField(label='Patient Gender', max_length=100)
-    patient_contact = forms.CharField(label='Patient Contact', max_length=100)
-    patient_dob = forms.CharField(label='Patient DOB', max_length=100)
-    appt_time = forms.DateTimeField(label='Appt Time')
-    appt_type = forms.CharField(label='Appt Type', max_length=100)
-    appt_clinic = forms.CharField(label='Clinic', max_length=100)
-
+class ChangepwForm(forms.Form):
+    oldpassword = forms.CharField(label='oldpassword', max_length=100)
+    newpassword = forms.CharField(label='newpassword', max_length=100)
+    confirmnewpassword = forms.CharField(label='confirmnewpassword', max_length=100)
