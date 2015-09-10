3a8bd994f8dba220c59699ad28c5d008ce407e6c
limjunyangleon <limjunyangleon@gmail.com>
2015-09-11 00:38:35 +0800

    Changed Today Patient's Appointments contact field.

--- static/ClearVision/js/angular/app-appointmentAnalysis.js
+++ static/ClearVision/js/angular/app-appointmentAnalysis.js
@@ -90,5 +90,49 @@ appointmentAnalysis.controller('AppointmentAnalysisCtrl', function ($scope) {
 
     $scope.showStackedChart($scope.testData);
 
+
+    $scope.pieData = [
+        {
+            "screening": 20,
+            "preEval": 20,
+            "surgery": 20,
+            "postOne": 20,
+            "postTwo": 20
+        }
+    ];
+
+    $scope.showPieChart = function (newData) {
+
+        $scope.pieChartOne = c3.generate({
+            bindto: '#pie1',
+            padding: {
+                top: 30,
+                right: 30,
+                bottom: 0,
+                left: 50
+            },
+            data: {
+                json: newData,
+                keys: {
+                    // x: 'name', // it's possible to specify 'x' when category axis
+                    x: 'apptType',
+                    value: ['turnUp', 'noShow', 'cancelled', 'undecided']
+                },
+                type: 'pie',
+                groups: [
+                    ['turnUp', 'noShow', 'cancelled', 'undecided']
+                ],
+                order: null
+            }
+        });
+
+        setTimeout(function () {
+            //$scope.marketingChart.toggle('convert');
+            //$scope.marketingChart.toggle('rate');
+        }, 50);
+
+    };
+
+
 });
 
