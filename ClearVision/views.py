4a7cd296d895a3937428c89f76432a781a0110e3
rotarycut <shermanyong@gmail.com>
2015-09-13 18:37:39 +0800

    Fixed date filter for stacked chart

--- ClearVision/views.py
+++ ClearVision/views.py
@@ -1064,10 +1064,10 @@ class AppointmentAnalysisStackedChart(viewsets.ReadOnlyModelViewSet):
         if customFilter == 'True':
             apptTypes = request.query_params.getlist('apptTypes')
             for eachApptType in apptTypes:
-                tillNowBlacklisted = Blacklist.objects.filter(timeBucket__date__date__month=month, timeBucket__timeslotType=eachApptType).values().count()
-                tillNowAttended = AttendedAppointment.objects.filter(attended=True, timeBucket__date__date__month=month, timeBucket__timeslotType=eachApptType).values().count()
-                totalPatientsForMonth = Appointment.objects.filter(timeBucket__date__date__month=month, timeBucket__timeslotType=eachApptType).values('patients').count()
-                totalCancelledForMonth = AssociatedPatientActions.objects.filter(appointment__timeBucket__date__date__month=month, cancelled=True, appointment__timeBucket__timeslotType=eachApptType).values().count()
+                tillNowBlacklisted = Blacklist.objects.filter(timeBucket__date__date__gte=startDate, timeBucket__date__date__lte=endDate, timeBucket__timeslotType=eachApptType).values().count()
+                tillNowAttended = AttendedAppointment.objects.filter(attended=True, timeBucket__date__date__gte=startDate, timeBucket__date__date__lte=endDate, timeBucket__timeslotType=eachApptType).values().count()
+                totalPatientsForMonth = Appointment.objects.filter(timeBucket__date__date__gte=startDate, timeBucket__date__date__lte=endDate, timeBucket__timeslotType=eachApptType).values('patients').count()
+                totalCancelledForMonth = AssociatedPatientActions.objects.filter(appointment__timeBucket__date__date__gte=startDate, appointment__timeBucket__date__date__lte=endDate, cancelled=True, appointment__timeBucket__timeslotType=eachApptType).values().count()
 
                 toAdd = {'apptType': eachApptType, 'Appeared': tillNowAttended, 'NoShow': tillNowBlacklisted, 'Cancelled': totalCancelledForMonth, 'Pending': totalPatientsForMonth-tillNowBlacklisted-tillNowAttended}
                 toReturnResponse.append(toAdd)
