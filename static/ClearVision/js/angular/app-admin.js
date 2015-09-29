5b67d67a59c185b61a5360affc699aaa87c57e4d
limjunyangleon <limjunyangleon@gmail.com>
2015-09-29 22:50:51 +0800

    Change password form validation working.

--- static/ClearVision/js/angular/app-admin.js
+++ static/ClearVision/js/angular/app-admin.js
@@ -1,22 +1,31 @@
 var appAdmin = angular.module('app.admin', []);
+
 appAdmin.controller('adminCtrl', function ($scope, $http, $modal) {
 
+    /* function to change password */
     $scope.changePassword = function () {
 
         $http.post('/Clearvision/_api/changepw/', {
-            "oldpassword": $scope.oldPw,
-            "newpassword": $scope.newPw,
-            "confirmnewpassword": $scope.newPwConfirmed
+            "oldpassword": $scope.fields.oldPassword,
+            "newpassword": $scope.fields.newPassword,
+            "confirmnewpassword": $scope.fields.confirmNewPassword
         })
             .success(function (data) {
-                console.log("Successful with http post");
+
+                if (data.error == "Wrong old password / New passwords do not match") {
+
+                    $scope.message = {error: "Old password is wrong. Please try again."};
+
+                } else {
+
+                    $scope.openSuccessModal();
+                    $scope.fields = {};
+                    $scope.changePwForm.$setPristine();
+                    $scope.changePwForm.$setUntouched();
+                }
 
                 $scope.message = data;
-                $scope.oldPw = "";
-                $scope.newPw = "";
-                $scope.newPwConfirmed = "";
 
-                $scope.openPwchangeModal('sm');
             })
 
             .error(function (data) {
@@ -24,15 +33,64 @@ appAdmin.controller('adminCtrl', function ($scope, $http, $modal) {
             });
     };
 
+    /* function to open change password modal */
     $scope.openPwchangeModal = function (size) {
 
         var modalInstance = $modal.open({
             animation: $scope.animationsEnabled,
             templateUrl: 'confirmPwchangeModal.html',
             controller: 'PwchangeModalInstanceCtrl',
-            size: size,
+            size: size
         });
     };
+
+    /* function to check if the change password form is valid */
+    $scope.isFormValid = function (formIsValid) {
+
+        // clears the error message
+        $scope.message = {'error': ''};
+
+        if (formIsValid) {
+            // all the form fields are filled
+
+            // check if new password & confirm password is the same
+            if ($scope.fields.newPassword == $scope.fields.confirmNewPassword) {
+
+                // correct match between passwords, sends post to backend
+                $scope.changePassword();
+
+            } else {
+
+                // wrong match between passwords, display error
+                $scope.message = {error: "New Password does not match. Please reconfirm your new password."};
+            }
+
+        } else {
+            // not all the form fields are filled, display error messages
+        }
+    };
+
+    $scope.fields = {};
+
+
+    /*******************************************************************************
+     modal codes
+     *******************************************************************************/
+
+
+    $scope.animationsEnabled = true;
+
+    /* function to open success modal when password is successfully changed */
+    $scope.openSuccessModal = function (size) {
+
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'mySuccessModalContent.html',
+            controller: 'ChangePwModalInstanceCtrl',
+            size: size
+        });
+    };
+
 });
 
 appAdmin.config(function ($httpProvider) {
@@ -40,15 +98,19 @@ appAdmin.config(function ($httpProvider) {
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 });
 
-appAdmin.controller('PwchangeModalInstanceCtrl', function ($scope, $modalInstance, $http) {
-
+appAdmin.controller('ChangePwModalInstanceCtrl', function ($scope, $modalInstance, $location) {
 
     $scope.composeSMS = function () {
 
     };
 
+    $scope.redirect = function () {
+        $scope.cancel();
+        $location.path('/');
+    };
+
     $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
+        $modalInstance.close();
     };
 });
 
