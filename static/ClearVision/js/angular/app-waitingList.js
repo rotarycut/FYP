98c818590965b928c69798dcbecc13951fddc5f8
limjunyangleon <limjunyangleon@gmail.com>
2015-10-04 05:05:55 +0800

    Show number of swappable appointments completed.

--- static/ClearVision/js/angular/app-waitingList.js
+++ static/ClearVision/js/angular/app-waitingList.js
@@ -1,6 +1,6 @@
 var appWaitingList = angular.module('app.waitingList', []);
 
-appWaitingList.controller('waitListCtrl', function ($scope, $http, $timeout, $interval, $log, updateNotificationCountSvc, getNotificationsSvc,$route) {
+appWaitingList.controller('waitListCtrl', function ($scope, $http, $timeout, $interval, $log, updateNotificationCountSvc, getNotificationsSvc, $route) {
 
     $scope.$route = $route;
     updateNotificationCountSvc.getScope($scope);
@@ -117,6 +117,19 @@ appWaitingList.controller('waitListCtrl', function ($scope, $http, $timeout, $in
     };
 
     $scope.lastThreeSeconds();
+
+    /* function to show number of appointments that can be swapped */
+    $scope.getNumberOfSwappableAppointments = function () {
+
+        $http.get('/Clearvision/_api/ViewSwappableNumber/')
+            .success(function (data) {
+                $scope.noOfSwappableAppts = data;
+            })
+            .error(function (data) {
+
+            });
+
+    };
 
     /*$interval(function () {
      $scope.getNotifications();
