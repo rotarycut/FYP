eceb8aeba108c7d990c348372a44b49e356007dd
rotarycut <shermanyong@gmail.com>
2015-10-05 22:24:06 +0800

    Changed doctor ordering

--- ClearVision/views.py
+++ ClearVision/views.py
@@ -1398,7 +1398,7 @@ class PatientQueue(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         response_data = AttendedAppointment.objects.filter(attended=True, timeBucket__date=datetime.now().date(),
                                                            timeBucket__date__gte=datetime.today()).\
-            values('patient_id', 'patient__name', 'originalAppt_id', 'last_modified', 'remarks')
+            values('patient_id', 'patient__name', 'originalAppt_id', 'last_modified', 'remarks', 'patient__contact')
 
         return Response(response_data)
 
