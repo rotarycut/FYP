d85e8f0eed7c0f7a25c4f1909f5a7a615f6ff39f
carinahuqunqun <qunqun.hu.2012@sis.smu.edu.sg>
2015-10-18 20:06:11 +0800

    doctor configuration up

--- static/ClearVision/js/angular/app-configurations.js
+++ static/ClearVision/js/angular/app-configurations.js
@@ -8,9 +8,24 @@ appConfig.controller('configCtrl', function ($scope, $http) {
         title: 'Title'
     };
 
+    $scope.showDocConfigForm = true;
+    $scope.showDocApptConfigForm = false;
+    $scope.docConfigActiveTab = "doctors-tab-active"
+
     $scope.listOfDoctors = ["Doctor Ho", "Doctor Goh", "Optometrist"];
 
-
     $scope.noOfDocs = $scope.listOfDoctors.length;
+
+    $scope.showDocView = function(){
+        $scope.showDocConfigForm = true;
+        $scope.showDocApptConfigForm = false;
+        $scope.docConfigActiveTab = "doctors-tab-active"
+    };
+
+    $scope.showDocApptView = function() {
+        $scope.showDocConfigForm = false;
+        $scope.showDocApptConfigForm = true;
+        $scope.docConfigActiveTab = "doc-appt-tab-active"
+    };
 
 });
