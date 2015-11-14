24547cc31bf2871657c6055260ff5ccc16e91976
limjunyangleon <limjunyangleon@gmail.com>
2015-11-15 01:05:13 +0800

    Create appointment updated.

--- static/ClearVision/js/angular/services/calendar/crud/createAppointment.js
+++ static/ClearVision/js/angular/services/calendar/crud/createAppointment.js
@@ -59,66 +59,68 @@ angular.module('post.appointment', [])
                 .success(function (appointment) {
 
                     $rootScope.spinner = {active: false};
+
                     showNotificationsSvc.notifySuccessTemplate('Appointment created successfully');
 
                     // disable iSchedule
                     disableIScheduleSvc.disableISchedule();
 
-                    console.log(self.scope.chosenDoctor);
-                    self.scope.changeView('month', 'myCalendar0');
-
-                    // check the appointment type of the created appointment
-                    switch (self.scope.fields.appointmentType) {
-
-                        case "Screening":
-                            var appointmentIndex = 0;
-
-                            // loop through all the screening events of the selected doctor
-                            angular.forEach(self.scope.selectedDoctor.drScreening.events, function (screeningAppointment) {
-
-                                // find if the created appointment is in the existing pool of screening events
-                                if (screeningAppointment.id === appointment.id) {
-
-                                    // remove the entire appointment from the calendar
-                                    self.scope.selectedDoctor.drScreening.events.splice(appointmentIndex, 1);
-                                }
-                                appointmentIndex++;
-                            });
-
-                            // add the updated appointment with addition of patient back into the calendar
-                            self.scope.selectedDoctor.drScreening.events.push(appointment);
-                            break;
-
-                        case "Pre Evaluation":
-                            var appointmentIndex = 0;
-
-                            angular.forEach(self.scope.selectedDoctor.drPreEval.events, function (preEvaluationAppointment) {
-
-                                if (preEvaluationAppointment.id === appointment.id) {
-                                    self.scope.selectedDoctor.drPreEval.events.splice(appointmentIndex, 1);
-                                }
-                                appointmentIndex++;
-                            });
-
-                            self.scope.selectedDoctor.drPreEval.events.push(appointment);
-                            break;
-
-                        case "Surgery":
-                            var appointmentIndex = 0;
-                            angular.forEach(self.scope.selectedDoctor.drSurgery.events, function (surgeryAppointment) {
-
-                                if (surgeryAppointment.id === appointment.id) {
-                                    self.scope.selectedDoctor.drSurgery.events.splice(appointmentIndex, 1);
-                                }
-                                appointmentIndex++;
-                            });
-
-                            self.scope.selectedDoctor.drSurgery.events.push(appointment);
-                            break;
-                    }
+                    // change view back to month view, this will retrieve the doctor appointments
+                    self.scope.changeView('month', self.scope.chosenDoctor.changeCalendar);
 
                     // clear the appointment form
                     clearFormSvc.clearForm();
+
+                    // check the appointment type of the created appointment
+                    /*
+                     switch (self.scope.fields.appointmentType) {
+
+                     case "Screening":
+                     var appointmentIndex = 0;
+
+                     // loop through all the screening events of the selected doctor
+                     angular.forEach(self.scope.selectedDoctor.drScreening.events, function (screeningAppointment) {
+
+                     // find if the created appointment is in the existing pool of screening events
+                     if (screeningAppointment.id === appointment.id) {
+
+                     // remove the entire appointment from the calendar
+                     self.scope.selectedDoctor.drScreening.events.splice(appointmentIndex, 1);
+                     }
+                     appointmentIndex++;
+                     });
+
+                     // add the updated appointment with addition of patient back into the calendar
+                     self.scope.selectedDoctor.drScreening.events.push(appointment);
+                     break;
+
+                     case "Pre Evaluation":
+                     var appointmentIndex = 0;
+
+                     angular.forEach(self.scope.selectedDoctor.drPreEval.events, function (preEvaluationAppointment) {
+
+                     if (preEvaluationAppointment.id === appointment.id) {
+                     self.scope.selectedDoctor.drPreEval.events.splice(appointmentIndex, 1);
+                     }
+                     appointmentIndex++;
+                     });
+
+                     self.scope.selectedDoctor.drPreEval.events.push(appointment);
+                     break;
+
+                     case "Surgery":
+                     var appointmentIndex = 0;
+                     angular.forEach(self.scope.selectedDoctor.drSurgery.events, function (surgeryAppointment) {
+
+                     if (surgeryAppointment.id === appointment.id) {
+                     self.scope.selectedDoctor.drSurgery.events.splice(appointmentIndex, 1);
+                     }
+                     appointmentIndex++;
+                     });
+
+                     self.scope.selectedDoctor.drSurgery.events.push(appointment);
+                     break;
+                     }*/
 
                 }).error(function (error) {
 
