74d7aa57cf9383a02ef550fa150f8301884ba44d
limjunyangleon <limjunyangleon@gmail.com>
2015-11-15 00:11:34 +0800

    Change calendar js updated and tidied.

--- static/ClearVision/js/angular/services/calendar/others/changeCalendar.js
+++ static/ClearVision/js/angular/services/calendar/others/changeCalendar.js
@@ -50,56 +50,6 @@ angular.module('change.calendar', [])
                     self.scope.enableISchedule();
                 }, 0);
 
-                // check if it is a doctor drop down selection
-                /*if (doctorDropDown) {
-
-                 // set global variable calendar number
-                 if (self.scope.fields.doctorAssigned.name == 'Dr Ho') {
-                 self.scope.selectedCalendar = 'myCalendar1';
-                 } else if (self.scope.fields.doctorAssigned.name == 'Dr Goh') {
-                 self.scope.selectedCalendar = 'myCalendar2';
-                 } else if (self.scope.fields.doctorAssigned.name == 'Optometrist') {
-                 self.scope.selectedCalendar = 'myCalendar3';
-                 }
-
-                 // check if the drop down selection is for which doctor
-                 if (self.scope.selectedCalendar == "myCalendar1") {
-
-                 // selected dr ho on drop down field
-                 self.scope.tabs[0].active = true;
-                 self.scope.tabs[1].active = false;
-                 self.scope.tabs[2].active = false;
-                 self.scope.changeSelectedDoctor(self.scope.doctorHoAppointments, '', self.scope.drHoPreEvaluations, self.scope.drHoSurgeries, '', self.scope.drHoPostSurgeries);
-
-                 } else if (self.scope.selectedCalendar == "myCalendar2") {
-
-                 // selected dr goh on drop down field
-                 self.scope.tabs[0].active = false;
-                 self.scope.tabs[1].active = true;
-                 self.scope.tabs[2].active = false
-                 self.scope.changeSelectedDoctor(self.scope.doctorGohAppointments, '', self.scope.drGohPreEvaluations, self.scope.drGohSurgeries, '', self.scope.drGohPostSurgeries);
-
-                 } else if (self.scope.selectedCalendar == "myCalendar3") {
-
-                 // selected optom on drop down field
-                 self.scope.tabs[0].active = false;
-                 self.scope.tabs[1].active = false;
-                 self.scope.tabs[2].active = true;
-                 self.scope.changeSelectedDoctor(self.scope.optomAppointments, self.scope.optomScreenings, '', '', self.scope.optomEyeCare, '');
-
-                 }
-
-                 // enable iSchedule
-                 $timeout(function () {
-                 self.scope.enableISchedule();
-                 }, 0);
-
-                 }
-
-                 // re render doctors calendars
-                 $('#drHoCalendar').fullCalendar('refetchEvents');
-                 $('#drGohCalendar').fullCalendar('refetchEvents');
-                 $('#optomCalendar').fullCalendar('refetchEvents');*/
 
             } else {
 
@@ -125,24 +75,6 @@ angular.module('change.calendar', [])
                 //self.scope.changeSelectedDoctor(self.scope.doctorGohAppointments, '', self.scope.drGohPreEvaluations, self.scope.drGohSurgeries, '', self.scope.drGohPostSurgeries);
 
             }
-
-        };
-
-        /*******************************************************************************
-         function to set current selected dr scope
-         *******************************************************************************/
-
-        self.changeSelectedDoctor = function (drSourceArray, drScreenings, drPreEval, drSurgery, drEyeCare, drPostSurgery) {
-
-            self.scope.selectedDoctor = {
-                //need to have eg: drHoCalendar
-                drAppointmentArray: drSourceArray,
-                drScreening: drScreenings,
-                drPreEval: drPreEval,
-                drSurgery: drSurgery,
-                drEyeCare: drEyeCare,
-                drPostSurgery: drPostSurgery
-            };
 
         };
 
