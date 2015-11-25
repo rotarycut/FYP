a0603ff2b8e9a6607da2addf87dda5213eb7199d
rotarycut <shermanyong@gmail.com>
2015-11-25 16:57:18 +0800

    Added in 2015 for dateprinter.py

--- ClearVision/dateprinter.py
+++ ClearVision/dateprinter.py
@@ -2,7 +2,7 @@ import calendar
 import datetime
 
 #year = 2015
-years = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]
+years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030]
 c = calendar.TextCalendar()
 dump = open("fixtures/availabletimeslotsdump15years.json", "w+")
 
